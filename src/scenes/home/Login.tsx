5ec42ea12aec1dd23108af6f5e759194392c2793
techiee3041 <doreenoms6699@gmail.com>
2024-04-03 13:19:43 +0300

    modify sign in component

--- src/scenes/home/Login.tsx
+++ src/scenes/home/Login.tsx
@@ -1,11 +1,47 @@
+import { Button } from "@material-tailwind/react";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { useState } from "react";
+import { auth } from "../../firebaseconfig";
+
 const Login = () => {
-    return (
-        <div className="w-full h-full">
-            <div className="w-3/5 bg-deep-orange-400 h-full top-40">
-                <h1>Login</h1>
-            </div>
-        </div>
-    );
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signIn = (e) => {
+    e.preventDefault();
+    signInWithEmailAndPassword(auth, email, password).then(
+      (userCredentials) => {
+        console.log(userCredentials);
+      }
+    ).catch((error) => {
+        console.log(error);
+    });
   };
-  
-  export default Login;
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <div className="w-2/5 bg-white py-20">
+        <h1>Welcome to the E-Wear</h1>
+        <p>Login or create an account to continue</p>
+        <form action="" onSubmit={signIn}>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Enter Your Email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Enter Your Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Button type="submit" className="block hover:opacity-90">Log In</Button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
